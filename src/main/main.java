ddf1131b72c664dfdec361d9037fb891e04170e1
bounacles <bounacles@gmail.com>
2021-11-23 11:41:44 +0100

    added src

---
+++ src/main/main.java
@@ -0,0 +1,16 @@
+package main;
+
+public class main {
+
+	public static void main(String[] args) {
+		System.out.println("funciono");
+		
+		int a =3;
+		
+		a += 4;
+		
+		System.out.println(a);
+
+	}
+
+}
